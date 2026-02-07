d0b08419e0cf533ad1b70b40f8baad0e9d15545e
cameronvarley <cam.avarley@gmail.com>
2026-02-07 12:01:30 -0500

    chore(commands): remove top-level commands index (use slash/context folders)

--- src/commands/index.ts
+++
@@ -1,7 +0,0 @@
-export * as admin from "./admin/index.js";
-export * as bite from "./bite/index.js";
-export * as pet from "./pet/index.js";
-export * as random from "./random/index.js";
-export * as perform from "./perform/index.js";
-export * as stats from "./stats/index.js";
-export * as user from "./user/index.js";
