664af2f318819ca117e8dddc4944d5b2a39eedf1
cameronvarley <cam.avarley@gmail.com>
2026-02-16 01:11:48 -0500

    fix: update version to 8.1.1 in package.json and package-lock.json style: center align children in RootLayout component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -49,9 +49,7 @@ export default async function RootLayout({
           <AppSidebar variant="floating" />
           <SidebarInset>
             <AppHeader />
-            <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-              {children}
-            </div>
+            <div className="flex justify-center p-4 pt-0">{children}</div>
           </SidebarInset>
         </SidebarProvider>
       </body>
