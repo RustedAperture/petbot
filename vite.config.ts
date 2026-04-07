0bf406fc011e291c16ee6fe4dcf8d185bfd2789f
cameronvarley <cam.avarley@gmail.com>
2026-04-07 19:29:27 -0400

    chore: upgrade vite to 8.0.7, remove vite-tsconfig-paths, use native tsconfigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,8 @@
 import { defineConfig } from "vitest/config";
-import { fileURLToPath } from "node:url";
 
 export default defineConfig({
   resolve: {
-    alias: [
-      {
-        find: /^@utils\/(.*)$/,
-        replacement: fileURLToPath(
-          new URL("./src/utilities/$1", import.meta.url),
-        ),
-      },
-      {
-        find: /^@components\/(.*)$/,
-        replacement: fileURLToPath(
-          new URL("./src/components/$1", import.meta.url),
-        ),
-      },
-      {
-        find: /^@commands\/(.*)$/,
-        replacement: fileURLToPath(
-          new URL("./src/commands/$1", import.meta.url),
-        ),
-      },
-      {
-        find: /^@types\/(.*)$/,
-        replacement: fileURLToPath(new URL("./src/types/$1", import.meta.url)),
-      },
-      {
-        find: /^@db\/(.*)$/,
-        replacement: fileURLToPath(new URL("./src/db/$1", import.meta.url)),
-      },
-      {
-        find: /^@logger$/,
-        replacement: fileURLToPath(new URL("./src/logger.js", import.meta.url)),
-      },
-    ],
+    tsconfigPaths: true,
   },
   test: {
     environment: "node",
